9d6d2368262c489c0a83f716fb0d6a7eae2209ab
nadi2611 <nadi.najarn@gmail.com>
2024-08-17 02:28:51 +0300

    add low level fat16_stat func

--- src/disk/disk.h
+++ src/disk/disk.h
@@ -5,8 +5,7 @@
 
 typedef unsigned int PEACHOS_DISK_TYPE;
 
-
-// Represents a real physical hard disk
+// Represents a real physical hard disk.
 #define PEACHOS_DISK_TYPE_REAL 0
 
 struct disk
@@ -16,7 +15,6 @@ struct disk
 
     // The id of the disk
     int id;
-
     struct filesystem* filesystem;
 
     // The private data of our filesystem
@@ -25,6 +23,7 @@ struct disk
 
 void disk_search_and_init();
 struct disk* disk_get(int index);
-int disk_read_block(struct disk* idisk, unsigned int lba, int total, void* buf);
+int disk_read_block(struct disk* idisk, unsigned int lba, int total, void* buffer);
+int disk_read_sector(int lba, int total, void* buf);
 
 #endif
