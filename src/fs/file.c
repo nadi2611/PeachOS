7ccf0c93f8aa85303630d0325ab65d839790b6ca
nadi2611 <nadi.najarn@gmail.com>
2024-08-17 02:18:20 +0300

    add fstat high level

--- src/fs/file.c
+++ src/fs/file.c
@@ -202,6 +202,23 @@ out:
     return res;
 }
 
+int fstat(int fd, struct file_stat* stat)
+{
+    int res = 0;
+    struct file_descriptor* desc = file_get_descriptor(fd);
+    if (!desc)
+    {
+        res = -EIO;
+        goto out;
+    }
+
+    res = desc->filesystem->stat(desc->disk, desc->private, stat);
+
+
+out:
+    return res;
+}
+
 int fseek(int fd, int offset, FILE_SEEK_MODE whence)
 {
     int res = 0;
@@ -240,5 +257,7 @@ int fread(void* ptr, uint32_t size, uint32_t nmemb, int fd)
 out:
     return res;
 }
+
+
 
 
